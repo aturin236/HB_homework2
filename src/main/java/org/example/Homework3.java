3c7c47c8471074c36832c62a3bbd2d16c7b0342d
aturin <aturin@avancore.ru>
2023-11-26 16:43:34 +0300

    homework 3 - обновлены задания 5 и 6

--- src/main/java/org/example/Homework3.java
+++ src/main/java/org/example/Homework3.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Homework3 {
     public static void runHomework() {
@@ -42,34 +40,71 @@ public class Homework3 {
         System.out.println("Найденное число = " + getNumberWithBubbleAndBinarySearch(array, key));
 
         System.out.print("\n");
-        System.out.println("Задание №6");
+        System.out.println("Задание №5");
 
         array = Service.getRandomArray(11, 20, false);
         key = Service.getRandomInt(20);
         System.out.println("Исходный массив - " + Arrays.toString(array));
         System.out.println("Число для поиска - " + key);
-        System.out.println("Индекс в исходном массиве = " + getIndex_homework3_2(array, key));
+        System.out.println("Найденное число = " + getNumberWithMergeBinarySearch(array, key));
+
+        System.out.print("\n");
+        System.out.println("Задание №6");
+
+        array = Service.getRandomArray(11, 10, false);
+        key = Service.getRandomInt(10);
+        System.out.println("Исходный массив - " + Arrays.toString(array));
+        System.out.println("Число для поиска - " + key);
+        System.out.println("Индекс в исходном массиве = " + Homework3_3_2.getIndex_homework3_2(array, key));
     }
 
-    private static int getIndex_homework3_2(int[] array, int key) {
-        Map<Integer, Integer> indexes = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            indexes.put(array[i], i);
+    private static int getNumberWithMergeBinarySearch(int[] array, int key) {
+        return getNumberByBinarySearch(
+                sortingByMerge(array),
+                key
+        );
+    }
+
+    private static int[] sortingByMerge(int[] array) {
+        if (array.length == 1) {
+            return array;
         }
 
-        return indexes.getOrDefault(
-                getNumberWithBubbleAndBinarySearch(array, key),
-                -1);
+        int mid = array.length / 2;
+
+        int[] arrayLeft = sortingByMerge(Arrays.copyOfRange(array, 0, mid));
+        int[] arrayRight = sortingByMerge(Arrays.copyOfRange(array, mid, array.length));
+
+        int[] arrayResult = new int[array.length];
+        int indexLeft = 0;
+        int indexRight = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (indexLeft == arrayLeft.length) {
+                arrayResult[i] = arrayRight[indexRight];
+                indexRight++;
+            } else if (indexRight == arrayRight.length) {
+                arrayResult[i] = arrayLeft[indexLeft];
+                indexLeft++;
+            } else if ((arrayLeft[indexLeft] > arrayRight[indexRight])) {
+                arrayResult[i] = arrayRight[indexRight];
+                indexRight++;
+            } else {
+                arrayResult[i] = arrayLeft[indexLeft];
+                indexLeft++;
+            }
+        }
+        return arrayResult;
     }
 
     private static int getNumberWithBubbleAndBinarySearch(int[] array, int key) {
-        return getIndexNumberByBinarySearch(
+        return getNumberByBinarySearch(
                 sortingByBubble(array),
                 key
         );
     }
 
-    private static int getIndexNumberByBinarySearch(int[] array, int key) {
+    private static int getNumberByBinarySearch(int[] array, int key) {
         int left = 0;
         int right = array.length - 1;
 
---
+++ src/main/java/org/example/Homework3_3_2.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class Homework3_3_2 {
+    public static final byte INDEX = 0;
+    public static final byte VALUE = 1;
+
+    public static int getIndex_homework3_2(int[] array, int key) {
+        int[][] array2D = new int[array.length][2];
+        for (int i = 0; i < array.length; i++) {
+            array2D[i] = new int[]{i, array[i]};
+        }
+
+        return getNumberByBinarySearch(
+                sortingByBubble(array2D),
+                key
+        );
+    }
+
+    private static int getNumberByBinarySearch(int[][] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid][VALUE] < key)
+                left = mid + 1;
+            else if (array[mid][VALUE] > key)
+                right = mid - 1;
+            else
+                return array[mid][INDEX];
+        }
+        return -1;
+    }
+
+    private static int[][] sortingByBubble(int[][] array) {
+        boolean weMustContinue = true;
+        while (weMustContinue) {
+            boolean haveExchange = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i-1][VALUE] > array[i][VALUE]) {
+                    int[] buffer = array[i];
+                    array[i] = array[i-1];
+                    array[i-1] = buffer;
+
+                    haveExchange = true;
+                }
+            }
+            if (!haveExchange) {
+                weMustContinue = false;
+            }
+        }
+        return array;
+    }
+
+}
